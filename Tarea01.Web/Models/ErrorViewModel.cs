3f44885348e4120318dafa86d140045e011ac75c
khyle <khyle@WIN_DEVBOX>
2025-09-17 20:57:51 -0700

    Add project files.

---
+++ Tarea01.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea01.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
